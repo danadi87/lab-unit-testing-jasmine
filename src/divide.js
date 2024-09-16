652c4a02afd7ed5d3a18ff9c82fde9873cecab22
Mirela Dragulescu <dragulescumirela@gmail.com>
2024-09-16 22:29:11 +0200

    Mirela's solution - forgot to fork initially so couldn't push the original version I did

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,12 @@
-function divide () {
-       
+function divide(num1, num2) {
+  let division = "";
+  if (num1 === undefined || num2 === undefined) {
+    return undefined;
+  } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return undefined;
+  } else {
+    return (division = num1 / num2);
+  }
 }
+console.log(divide(8, 4));
+console.log(divide(0, 4));
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,28 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+describe("Iteration 2 | Divide", () => {
+  describe("Function - divide", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
 
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+    it("should take two numbers as arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+    it("should return the division of two numbers", () => {
+      expect(divide(6, 3).toEqual(2));
+      expect(divide(20, 4).toEqual(5));
+    });
+    it("should return undefined if any of the arguments is not a number", () => {
+      expect(divide("147", 1)).toEqual(undefined);
+      expect(divide(3, "25")).toEqual(undefined);
+      expect(divide("34", "678")).toEqual(undefined);
+      expect(divide(true, 3)).toEqual(undefined);
+    });
+    it("should return undefined if any of the arguments are not provided", () => {
+      expect(divide(1)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+    });
+  });
+});
